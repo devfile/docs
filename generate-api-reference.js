a7173b8e5c6b31ced23f154092e898c46b8ce46e
Kevin Owen <kowen@redhat.com>
2021-04-19 13:46:13 -0400

    Restructure site for multiple sets of documentation

--- generate-api-reference.js
+++ generate-api-reference.js
@@ -5,23 +5,23 @@ const bootprintJsonSchema = require('bootprint-json-schema')
 const bootprintConfig = require('./reference-generator/config.js')
 const fs = require('fs');
 
-fs.readdir('./docs/modules/user-guide/attachments/jsonschemas/', (err, files) => {
+fs.readdir('./docs/modules/spec/attachments/jsonschemas/', (err, files) => {
     if (err) throw err;
     files.forEach((version) => {
         var glob = require('glob-fs')();
-        var schemas = glob.readdirSync('./docs/modules/user-guide/attachments/jsonschemas/' + version + '/devfile*.json');
+        var schemas = glob.readdirSync('./docs/modules/spec/attachments/jsonschemas/' + version + '/devfile*.json');
         if (! schemas.empty) {
             var schemaPath = schemas[0]
-            var apiReferencePath = './docs/modules/user-guide/attachments/api-reference/' + version + '/'
+            var apiReferencePath = './docs/modules/spec/attachments/api-reference/' + version + '/'
             console.log("Generating Api reference:\n  - from schema: " + schemaPath + '\n  - to folder: ' + apiReferencePath)
             new Bootprint(bootprintJsonSchema, bootprintConfig)
             .run(schemaPath, apiReferencePath)
             .then((result) => {
                 try {
-                    fs.mkdirSync(`./docs/modules/user-guide/examples/api-reference/${version}/`, { recursive: true })    
+                    fs.mkdirSync(`./docs/modules/spec/examples/api-reference/${version}/`, { recursive: true })    
                 } catch(e) { console.error(e)}
                 fs.writeFileSync(
-                    `./docs/modules/user-guide/examples/api-reference/${version}/body.html`,
+                    `./docs/modules/spec/examples/api-reference/${version}/body.html`,
                     `
                     <a href="_attachments/jsonschemas/${version}/devfile.json">Download current the JSON Schema</a>
                     <iframe src="../_attachments/api-reference/${version}/index.html" style="border:none;width: 100%;min-height:50em;height:-webkit-fill-available;"></iframe>
